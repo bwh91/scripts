12a3d872cd173813866175b68a19bb6771fb502c
Brent <brent@novastel.la>
2016-10-19 23:35:02 -0400

    added script

---
+++ install-script.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python3-venv 
+sudo apt-get install python3-pip
+sudo apt-get install vim
+sudo apt-get install tlp
+sudo apt-get install chromium-browser
+sudo apt-get install git
+sudo apt-get install terminator
+
